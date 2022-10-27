1fbdb5ff2fe4778f34667cbabfbcb2deb169eb1e
Note-72 <Note-72@FULLTECHPRO-NOT>
2022-10-27 16:40:56 -0300

    Add project files.

---
+++ projeto_github_api/DadosRetorno.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitHub_Api
+{
+    class DadosRetorno
+    {
+        public string login { get; set; }
+        public string avatar_url { get; set; }
+        public string repos_url { get; set; }
+        public bool site_admin { get; set; }
+        public string location { get; set; }
+        public string bio { get; set; }
+        public string followers { get; set; }
+        public string following { get; set; }
+        public string created_at { get; set; }
+        public string updated_at { get; set; }
+
+        public DateTime ArrumarData(string data)
+        {
+
+            DateTime dataArrumada = DateTime.ParseExact(data, "yyyy-MM-ddTHH:mm:ssZ",
+                                   System.Globalization.CultureInfo.InvariantCulture);
+
+            return dataArrumada;
+        }
+    }
+}
---
+++ projeto_github_api/Program.cs
@@ -0,0 +1,59 @@
+﻿using RestSharp;
+using RestSharp.Deserializers;
+using System;
+using System.Text;
+
+namespace GitHub_Api // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string user = "";
+
+            Console.WriteLine("Algoritmo que procura dados no github!");
+            Console.Write("Por favor insira o seu usuário: ");
+            user = Console.ReadLine();
+
+            RestClient client = new RestClient($"https://api.github.com/users/{user}");
+            RestRequest request = new RestRequest(Method.GET);
+
+            IRestResponse response = client.Execute(request);
+
+            Console.WriteLine();
+
+            if (response.StatusCode == System.Net.HttpStatusCode.BadRequest)
+            {
+                Console.WriteLine($"Ocorreu um problema na sua requisição!\n\n{response.Content}\n\n{response.StatusCode}\n\n{response.ContentLength}");
+            }
+            else
+            {
+                DadosRetorno dadosRetorno = new JsonDeserializer().Deserialize<DadosRetorno>(response);
+                
+                var sb = new System.Text.StringBuilder();
+
+                if (dadosRetorno.login is null)
+                {
+                    Console.WriteLine("A api retornou um valor, mas não os dados desejados!: " + response.Content);
+                }
+                else
+                {
+                    sb.AppendLine($"Login: {dadosRetorno.login}");
+                    sb.AppendLine($"Avatar Link: {dadosRetorno.avatar_url}");
+                    sb.AppendLine($"Repositórios Link: {dadosRetorno.repos_url}");
+                    sb.AppendLine($"Adm do Site: {dadosRetorno.site_admin}");
+                    sb.AppendLine($"Local: {dadosRetorno.location}");
+                    sb.AppendLine($"Bio: {dadosRetorno.bio}");
+                    sb.AppendLine($"Quantidade de Seguidores: {dadosRetorno.followers}");
+                    sb.AppendLine($"Seguindo: {dadosRetorno.following}");
+                    sb.AppendLine($"Criado em: {dadosRetorno.ArrumarData(dadosRetorno.created_at)}");
+                    sb.AppendLine($"Atualizado em: {dadosRetorno.ArrumarData(dadosRetorno.updated_at)}");
+
+                    Console.WriteLine(sb);
+                }
+
+
+            }
+        }
+    }
+}
